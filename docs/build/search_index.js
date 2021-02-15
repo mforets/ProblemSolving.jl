f332b79798b70e485bc00f80b1e97125692ac24b
mforets <mforets@gmail.com>
2021-02-15 04:15:51 -0300

    update gitignore

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"hints.html#Hints","page":"Hints","title":"Hints","text":"","category":"section"},{"location":"problems.html#Problems","page":"Problems","title":"Problems","text":"","category":"section"},{"location":"solutions.html#Solutions","page":"Solutions","title":"Solutions","text":"","category":"section"},{"location":"index.html#Introduction","page":"Introduction","title":"Introduction","text":"","category":"section"}]
-}
