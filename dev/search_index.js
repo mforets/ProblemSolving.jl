cb7ff9d0ab379d0304c8f8e860fc05b3f299503f
Documenter.jl <documenter@juliadocs.github.io>
2021-02-15 07:16:37 +0000

    build based on 8ee6523

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"hints/#Hints","page":"Hints","title":"Hints","text":"","category":"section"},{"location":"problems/#Problems","page":"Problems","title":"Problems","text":"","category":"section"},{"location":"solutions/#Solutions","page":"Solutions","title":"Solutions","text":"","category":"section"},{"location":"#Introduction","page":"Introduction","title":"Introduction","text":"","category":"section"}]
+}
